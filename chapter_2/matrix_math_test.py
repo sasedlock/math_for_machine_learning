ad0d7739b7470c2431f7510dc9ed311cb6147d9e
Sean Sedlock <sean.sedlock@gmail.com>
2023-06-22 22:20:34 -0500

    Removing unnecessary tests

--- chapter_2/matrix_math_test.py
+++ chapter_2/matrix_math_test.py
@@ -2,12 +2,6 @@ import chapter_2.matrix_math as matrix_math
 import unittest
 
 class Test_TestMatrixMath(unittest.TestCase):
-    def test_validateArrayEquality(self):
-        self.assertEqual([1,2],[1,2])
-
-    def test_ValidateMatrixEquality(self):
-        self.assertEqual([[1,2],[3,4]],[[1,2],[3,4]])
-
     def test_add_scalar(self):
         expected = [3]
         actual = matrix_math.add([1],[2])
@@ -22,3 +16,5 @@ class Test_TestMatrixMath(unittest.TestCase):
         expected = [[2,4,6],[8,10,12],[14,16,18]]
         actual = matrix_math.add([[1,2,3],[4,5,6],[7,8,9]],[[1,2,3],[4,5,6],[7,8,9]])
         self.assertEqual(expected,actual)
+
+    
